1163db613bf9b5a96678d45393f601f4fcf42f94
Sigurd Hofsmo Jakobsen <sigurd.jakobsen@sintef.no>
2023-10-25 13:48:00 +0200

    The code now runs correctly while considering capacity
    On branch 9-consider-reserve-capacity

--- test/test_fasad_calc_CINELDI.jl
+++ test/test_fasad_calc_CINELDI.jl
@@ -16,3 +16,13 @@ ENS_sum = sum(ENS+ENSt;dims=2)
 ENS_sum_target = [0.18; 0.94; 0.11; 0.86]
 epsilon = sum(ENS_sum_target)*1/100 # [kWh]. I take 1% of expected total interrupted energy as maximum error in calculation
 @test (abs(sum(ENS_sum - ENS_sum_target))<epsilon)
+
+# Check if it works when we consider capacity
+res, rest = relrad_calc(cost_functions, network)
+ENS = res.ENS
+ENSt = rest.ENS
+ENS_sum = sum(ENS+ENSt;dims=2)
+
+ENS_sum_target = [0.18; 2.54; 0.11; 0.86]
+epsilon = sum(ENS_sum_target)*1/100 # [kWh]. I take 1% of expected total interrupted energy as maximum error in calculation
+@test (abs(sum(ENS_sum - ENS_sum_target))<epsilon)
