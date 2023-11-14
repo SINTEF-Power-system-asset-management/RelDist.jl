f412f7f83bfe94f3c54c4b85ebb37c33f68025cf
Sigurd Hofsmo Jakobsen <sigurd.jakobsen@sintef.no>
2023-11-14 14:12:24 +0100

    Added test for switch failures
    I added tests for showing that switch failures are handled correctly.
    
    On GH-15

--- test/test_fasad_calc_CINELDI.jl
+++ test/test_fasad_calc_CINELDI.jl
@@ -7,8 +7,10 @@ cost_filename = joinpath(@__DIR__, "../databases/cost_functions_dummy.json")
 cost_functions = read_cost_functions(cost_filename)
 network =  RadialPowerGraph(network_filename)
 
+conf = RelDistConf(traverse=Traverse(consider_cap=false),
+                   failures=Failures(switch_failures=true))
 
-res, _, _ = relrad_calc(cost_functions, network, RelDistConf(traverse=Traverse(consider_cap=false)))
+res, _, _ = relrad_calc(cost_functions, network, conf)
 ENS = res["base"].ENS
 ENSt = res["temp"].ENS
 ENS_sum = sum(ENS+ENSt;dims=2)
@@ -16,6 +18,14 @@ ENS_sum = sum(ENS+ENSt;dims=2)
 ENS_sum_target = [0.18; 0.94; 0.11; 0.86]
 epsilon = sum(ENS_sum_target)*1/100 # [kWh]. I take 1% of expected total interrupted energy as maximum error in calculation
 @test (abs(sum(ENS_sum - ENS_sum_target))<epsilon)
+
+ENS = Dict("downstream" => [3, 0.05, 0.0375, 0.025],
+           "upstream" => [0.0625, 0.05, 1.8, 1.2])
+
+# Check upstream and downstream switch failure on line 3
+for key in keys(ENS)
+    @test abs(sum(res[key].ENS[:, 3])-sum(ENS[key]))<epsilon
+end
 
 # Check if it works when we consider capacity
 res, _, _ = relrad_calc(cost_functions, network)
@@ -26,3 +36,4 @@ ENS_sum = sum(ENS+ENSt;dims=2)
 ENS_sum_target = [0.18; 2.54; 0.11; 0.86]
 epsilon = sum(ENS_sum_target)*1/100 # [kWh]. I take 1% of expected total interrupted energy as maximum error in calculation
 @test (abs(sum(ENS_sum - ENS_sum_target))<epsilon)
+
