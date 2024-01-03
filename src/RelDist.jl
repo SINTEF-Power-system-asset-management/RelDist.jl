1ea1b182cdc50993f6f878c4ba8b24dd994bb061
Sigurd Hofsmo Jakobsen <sigurd.jakobsen@sintef.no>
2024-01-03 10:50:48 +0100

    Removed pref and interruption things
    I commented out things related to pref and interruptions.

--- src/RelDist.jl
+++ src/RelDist.jl
@@ -19,7 +19,7 @@ include("relindices.jl")
 export calculate_kile, set_rel_res!, f_lin, f_piece
 
 include("relrad_io.jl")
-export read_interruption, read_cost_functions, read_correction_factors_from_csv, read_loadprofile
+export read_cost_functions, read_correction_factors_from_csv, read_loadprofile
 
 include("constants.jl")
 export MONTH_FACTORS, DAY_FACTORS, HOUR_FACTORS, COST_FUN, LOAD_PROFILES, TEMPERATURE_TABLE, TEMPERATURE_AVERAGE, REFERENCETIME_TABLE
