89354a5bb5fefe7ba625e6227ba45e4b4cfd4a4c
Sigurd Hofsmo Jakobsen <sigurd.jakobsen@sintef.no>
2024-04-04 07:32:12 +0200

    Uncomment some things in runtest for faster testing.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,17 @@
 using RelDist
 using Test
 
-@testset "Testing IO" begin
-    include("test_IO.jl")
-end
+# @testset "Testing IO" begin
+    # include("test_IO.jl")
+# end
 
-@testset "Testing branch types" begin
-    include("test_branch_types.jl")
-end
+# @testset "Testing branch types" begin
+    # include("test_branch_types.jl")
+# end
 
-@testset "Testing configuration structs" begin
-    include("test_configurations.jl")
-end
+# @testset "Testing configuration structs" begin
+    # include("test_configurations.jl")
+# end
 
 # This is using the interruptionn format that we have not used
 # for years.
@@ -23,9 +23,9 @@ end
     # include("test_pref_calc.jl")
 # end
 
-@testset "Testing Interruption costs calculation on basic radial net" begin
-    include("test_relrad_calc_CINELDI.jl")
-end
+# @testset "Testing Interruption costs calculation on basic radial net" begin
+    # include("test_relrad_calc_CINELDI.jl")
+# end
 
 @testset "Testing Interruption costs calculation on fasad net" begin
     include("test_fasad_calc_CINELDI.jl")
