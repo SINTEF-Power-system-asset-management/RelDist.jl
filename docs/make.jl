19096f265c66a076bae0431f187a5cc73dcfe209
Iver Karlsbakk Småge <iver.smage@sintef.no>
2024-07-25 14:50:29 +0200

    run formatter

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,4 @@
 using Documenter, RelDist
 
-makedocs(sitename="RelDist documentation")
-deploydocs(repo="github.com/SINTEF-Power-system-asset-management/RelDist.jl.git")
+makedocs(sitename = "RelDist documentation")
+deploydocs(repo = "github.com/SINTEF-Power-system-asset-management/RelDist.jl.git")
