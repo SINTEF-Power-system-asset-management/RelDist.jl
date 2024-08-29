6ecc05e74310a2e324987ebfac8fb4b4e859e17b
Sigurd Hofsmo Jakobsen <sigurd.jakobsen@sintef.no>
2024-08-29 14:00:42 +0200

    Changed CINELDI example to first read in the case

--- examples/CINELDI/run_cineldi.jl
+++ examples/CINELDI/run_cineldi.jl
@@ -1,6 +1,5 @@
 using RelDist
 using SintPowerCase
-using SintPowerGraphs
 using DataFrames
 
 network_filename = joinpath(@__DIR__, "CINELDI.toml")
@@ -8,7 +7,9 @@ cost_filename = joinpath(@__DIR__, "../../databases/cost_functions.json")
 
 cost_functions = read_cost_functions(cost_filename)
 
-network = Network(network_filename)
+case = Case(network_filename)
+
+network = Network(case)
 
 t = compress_relrad(network)
 res = transform_relrad_data(network, t, cost_functions)
