d37de4b20f4943eed4fbb57967fb59a60d5e3bd5
Sigurd Hofsmo Jakobsen <sigurd.jakobsen@sintef.no>
2024-09-18 13:46:11 +0200

    Finished example adding battery

--- examples/CINELDI/run_cineldi.jl
+++ examples/CINELDI/run_cineldi.jl
@@ -15,6 +15,17 @@ t = compress_relrad(network)
 res = transform_relrad_data(network, t, cost_functions)
 
 # Add a battery on bus 25
-temp_gen = DataFrame(copy(case.gen[end, :]))
-temp_gen.bus = 25
-# temp_gen.ID = 
+case.gen.E .= Inf
+temp_gen = DataFrame(case.gen[end, :])
+temp_gen.bus .= "25"
+temp_gen.ID .= "B1"
+temp_gen.external .= false
+temp_gen.E .= 2
+temp_gen.Pmax .= 10
+
+case.gen = vcat(case.gen, temp_gen)
+
+network = Network(case)
+
+t = compress_relrad(network)
+res_b = transform_relrad_data(network, t, cost_functions)
